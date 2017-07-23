3975072fea160ebb64a5117079b2961b898d093b
stPhoenix <bohdansapovsky@gmail.com>
2017-07-23 15:37:06 +0300

    Add some settings and create first model

--- project/news/models.py
+++ project/news/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+TITLE_MAX_LENGTH = 90
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=TITLE_MAX_LENGTH)
+    pub_date = models.DateTimeField()
+    article_text = models.TextField()
+    image = models.ImageField()
