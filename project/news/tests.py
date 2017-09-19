7d3514566ceda0a60430c44fa221ed6732f80901
stPhoenix <bohdansapovsky@gmail.com>
2017-09-19 10:42:03 +0300

    Add first test for news:Index

--- project/news/tests.py
+++ project/news/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from django.template.loader import render_to_string
+#from news.views import Index
+from .views import Index
 # Create your tests here.
+
+
+class IndexTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+    def test_index_page(self):
+        result = self.client.get('/')
+        rendered_html = render_to_string('news/index.html')
+        self.assertEqual(200, result.status_code)
+        self.assertAlmostEqual(result.body, rendered_html)
