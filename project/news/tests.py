2d4b9fadb2cb6330565451738ff0150cb0e5a6d2
stPhoenix <bohdansapovsky@gmail.com>
2017-09-25 11:02:56 +0300

    Add test for news:Add comment

--- project/news/tests.py
+++ project/news/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from django.template.loader import render_to_string
 from django.utils import timezone
+from django.contrib.auth.models import AnonymousUser
 from django.shortcuts import render
 #from news.views import Index
 from .views import Index
@@ -42,3 +43,24 @@ class DetailTest(TestCase):
         self.assertContains(response, 'test category')
         self.assertContains(response, 'Text for test article')
         self.assertContains(response, 'None.img')
+
+
+class CommentingTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        category = Category.objects.create(name='test category')
+        category.save()
+        article = Article.objects.create(title='Test article',
+                                         pub_date=timezone.now(),
+                                         article_text='Text for test article',
+                                         image='None.img',
+                                         category=category)
+        article.save()
+
+    def test_commenting(self):
+        request = self.client.post('/news/1/add_comment', {'comment_text': 'test comment'}, follow=True)
+        request.user = AnonymousUser
+        self.assertEqual(request.status_code, 200)
+        self.assertContains(request, 'test comment')
+
