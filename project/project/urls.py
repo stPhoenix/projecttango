855f22e4029b2b54c424812c64a35f4f9ba7d5b2
bohdansapovsky <bohdansapovsky@gmail.com>
2017-11-28 05:31:24 -0500

    Add login.html to new design

--- project/project/urls.py
+++ project/project/urls.py
@@ -19,13 +19,14 @@ from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from django.conf import settings
 from news import views  # Django freaks because no top level import like from ../news import views
+from project.settings import TEMPLATE_PREFIX as TP
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^news/', include('news.urls')),
     url(r'^account/', include('account.urls')),
     url(r'^$', views.Index.as_view(), name='index'),
-    url(r'^login/$', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='account/'+TP+'/login.html'), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
 ]
