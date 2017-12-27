35fbfa166cefe5a3ecbbe4bbab0f76b6f4775d55
stPhoenix <bohdansapovsky@gmail.com>
2017-12-27 22:25:55 +0200

    Models.py : Removed unneeded import. Add default value for image in Article Added default.jpg as default article_image Style.css : Added @media for different devices Base.html : Started remodeling design for different devices

--- project/news/models.py
+++ project/news/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
-
 # Create your models here.
 TITLE_MAX_LENGTH = 90
 
@@ -13,12 +11,13 @@ class Article(models.Model):
     title = models.CharField(max_length=TITLE_MAX_LENGTH)
     pub_date = models.DateTimeField()
     article_text = models.TextField()
-    image = models.ImageField(upload_to='article_images/')
+    image = models.ImageField(upload_to='article_images/', default='article_images/default.jpg')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return "\n Title: %s \n Publication date: %s \n Category: %s \n Text: %s \n Image: %s" % \
                 (self.title, self.pub_date, self.category.name, self.article_text, self.image.url)
+
 
 class Tags(models.Model):
     tag = models.CharField(max_length=30)
