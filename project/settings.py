ca84b98e69a4fc04de9a26c163b9ac0fc31d38a8
Pavlo <pavlo.stehantsev@gmail.com>
2018-01-04 00:10:47 +0200

    Added STATIC_ROOT and STATICFILES_DIRS

--- project/settings.py
+++ project/settings.py
@@ -126,9 +126,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
-                    ]
+STATICFILES_DIRS = [os.path.join(PROJECT_ROOT, 'static'),]
 
 MEDIA_URL = '/media/'
 
