54718b0f658d1f5fea3f59f243144c688aec32ad
bohdansapovsky <bohdansapovsky@gmail.com>
2018-01-03 17:10:48 -0500

    Add vars for collecting static files

--- project/settings.py
+++ project/settings.py
@@ -120,9 +120,15 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, 'static'),
+]
 
 MEDIA_URL = '/media/'
 
