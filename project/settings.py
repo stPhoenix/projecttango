37d4f58cee577a7811f56aef7db84cea34514470
bohdansapovsky <bohdansapovsky@gmail.com>
2018-01-04 04:47:25 -0500

    Fixed mispelling

--- project/settings.py
+++ project/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
