fba694b3bc41764687f7596903d1b1c5a0bdb881
stPhoenix <bohdansapovsky@gmail.com>
2018-01-17 20:51:05 +0200

    Added basic admin panel

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from news.models import Article, Category, Tags
 # Register your models here.
+admin.site.register(Article)
+admin.site.register(Category)
+admin.site.register(Tags)
