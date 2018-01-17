d735c2a97f9fbba7af84e318f5ca7e3f257c7b70
stPhoenix <bohdansapovsky@gmail.com>
2018-01-17 21:14:15 +0200

    Admin panel rev2

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from news.models import Article, Category, Tags
 # Register your models here.
-admin.site.register(Article)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'pub_date')
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('tag', 'article_title')
+
+    def article_title(self, obj):
+        return obj.article.title
+
+
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Category)
-admin.site.register(Tags)
+admin.site.register(Tags, TagAdmin)
